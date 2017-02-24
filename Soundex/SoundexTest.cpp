43426dafe75a398dc626c240bf9dae06993a78b9
Emory Freitas <emory.freitas@itriad.org.br>
2017-02-24 11:49:23 -0400

    Finish first test - RetainsSoleLetterOfOneLetterWord

--- Soundex/SoundexTest.cpp
+++ Soundex/SoundexTest.cpp
@@ -1,8 +1,16 @@
-class Soundex {};
-
 #include <gtest/gtest.h>
+#include <iostream>
 
-TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) { Soundex soundex; }
+class Soundex {
+public:
+  std::string encode(const std::string &word) const { return "A"; }
+};
+
+TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
+  Soundex soundex;
+  auto encoded = soundex.encode("A");
+  ASSERT_EQ(encoded, "A");
+}
 
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
