0b5e0c3118a6a78021b0fbdd2da2697e6a3a20e6
Emory Freitas <emory.freitas@itriad.org.br>
2017-03-02 10:28:48 -0400

    Adding test to get more than one digit

--- Soundex/SoundexTest.cpp
+++ Soundex/SoundexTest.cpp
@@ -22,6 +22,14 @@ TEST_F(SoundexEncoding, ReplacesConsonantsWithAppropriateDigits) {
   ASSERT_EQ(soundex.encode("Ad"), "A300");
 }
 
+TEST_F(SoundexEncoding, IgnoresNonAlphabetics) {
+  ASSERT_EQ(soundex.encode("A#"), "A000");
+}
+
+TEST_F(SoundexEncoding, ReplacesMultipleConsonantsWithDigits) {
+  ASSERT_EQ(soundex.encode("Acdl"), "A234");
+}
+
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
